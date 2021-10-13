b4ba9d9ce622f7ea89b264f0216f60f2fd4e3b2b
Shreyanand <shanand@redhat.com>
2021-10-13 20:44:49 +0000

    Update config files for the notebooks

--- notebooks/demo2/config.py
+++ notebooks/demo2/config.py
@@ -7,10 +7,9 @@ import torch
 STAGE = "extract"  # "extract" | "curate "
 SEED = 42
 
-ROOT = pathlib.Path(src.__file__).resolve().parent.parent
-CONFIG_FOLDER = ROOT / "config"
+ROOT = pathlib.Path(__file__).resolve().parent.parent.parent
+CONFIG_FOLDER = ROOT
 CHECKPOINT_FOLDER = ROOT / "checkpoint"
-# the data for demo notebooks is located at sample_data directory
 DATA_FOLDER = ROOT / "data"
 BASE_PDF_FOLDER = DATA_FOLDER / "pdfs"
 BASE_ANNOTATION_FOLDER = DATA_FOLDER / "annotations"
@@ -18,19 +17,8 @@ BASE_EXTRACTION_FOLDER = DATA_FOLDER / "extraction"
 BASE_CURATION_FOLDER = DATA_FOLDER / "curation"
 
 
-# If installed as package, BASE_EXTRACTION_FOLDER will point to site-packages
-# So we need to update how config params are passed
-# if not os.path.exists(BASE_EXTRACTION_FOLDER):
-#     os.mkdir(BASE_EXTRACTION_FOLDER)
-# if not os.path.exists(BASE_CURATION_FOLDER):
-#     os.mkdir(BASE_CURATION_FOLDER)
-
-ckpt = "icdar_19b2_v2.pth" #if "cpu" in torch.__version__ else "icdar_19b2.pth"
-config_file = (
-    "cascade_mask_rcnn_hrnetv2p_w32_20e_coco.py"
-    # if "cpu" in torch.__version__
-    # else "cascade_mask_rcnn_hrnetv2p_w32_20e.py"
-)
+ckpt = "icdar_19b2_v2.pth"
+config_file = "cascade_mask_rcnn_hrnetv2p_w32_20e_coco.py"
 PDFTableExtractor_kwargs = {
     "batch_size": -1,
     "cscdtabnet_config": CONFIG_FOLDER / config_file,
@@ -42,8 +30,6 @@ PDFTableExtractor_kwargs = {
 # PDFTextExtractor
 PDFTextExtractor_kwargs = {
     "min_paragraph_length": 30,
-    # Set to  ANNOTATION_FOLDER if you want to extract just pdfs mentioned in the annotations
-    # Set to None to extract all pdfs in pdf folder (for production stage)
     "annotation_folder": None,
     "skip_extracted_files": False,
 }
