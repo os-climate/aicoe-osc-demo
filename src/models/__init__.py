7c0ff1340fc7bb0812bb07e3615d02b6294403cb
Karanraj Chauhan <kachau@redhat.com>
2021-11-01 12:47:11 -0400

    Add inference source code
    Squashed:
    Add inference classes in init

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,3 +2,4 @@
 from .farm_trainer import FARMTrainer
 from .qa_farm_trainer import QAFARMTrainer
 from .trainer_optuna import TrainerOptuna
+from .relevance_infer import TextRelevanceInfer, TableRelevanceInfer
