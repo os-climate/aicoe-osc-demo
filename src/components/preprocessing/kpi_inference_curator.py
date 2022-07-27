99606a6603a76c0c95a31745651d8f58ee9179fc
MichaelTiemannOSC <mtiemann@os-climate.org>
2022-07-27 15:49:23 +0000

    Make black text format checker happy.
    Signed-off-by: MichaelTiemannOSC <mtiemann@os-climate.org>

--- src/components/preprocessing/kpi_inference_curator.py
+++ src/components/preprocessing/kpi_inference_curator.py
@@ -150,9 +150,9 @@ class TextKPIInferenceCurator(BaseKPIInferenceCurator):
         # if single relevant paragraphs, then assuming only has single source page (fair enough)
         df_single = df[df["relevant_paragraphs"].apply(len) == 1].copy()
         df_single.loc[:, "source_page"] = df_single["source_page"].apply(lambda x: x[0])
-        df_single.loc[:, "relevant_paragraphs"] = df_single["relevant_paragraphs"].apply(
-            lambda x: x[0]
-        )
+        df_single.loc[:, "relevant_paragraphs"] = df_single[
+            "relevant_paragraphs"
+        ].apply(lambda x: x[0])
 
         # Otherwise
         df_multi = df[df["relevant_paragraphs"].apply(len) > 1]
